8b643808449ff2f0ad6f59295081683f2ba45722
warda195 <ws1952007@gmail.com>
2025-12-28 10:36:30 +0500

    Initial commit / fix image paths

---
+++ script.js
@@ -0,0 +1,18 @@
+
+window.onload = function(){
+    alert("Welcome to Happy Paws 🐾");
+}
+
+
+function validateForm(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let msg = document.getElementById("msg").value;
+
+    if(name=="" || email=="" || msg==""){
+        alert("Please fill all fields!");
+        return false;
+    }
+    alert("Thank you for contacting Happy Paws 🐾");
+    return true;
+}
